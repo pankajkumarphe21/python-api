92349a7e3e7381ac4b4c34de07417e5d7c944b66
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-01-07 12:06:36 +0530

    commit

--- p.py
+++ p.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-# from flask_cors import CORS
+from flask_cors import CORS
 from joblib import load
 import numpy as np
 from dotenv import load_dotenv
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": os.getenv('BACKEND_URL')}})
+CORS(app, resources={r"/*": {"origins": os.getenv('BACKEND_URL')}})
 
 model = load('file.joblib')
 
@@ -19,12 +19,12 @@ def home():
 def getUser(userId):
     return jsonify(f"Hi {userId}")
 
-# @app.route("/predict/<input>",methods=['POST'])
-# def predict(input):
-#     data = [int(input)]
-#     inputs = np.array(data).reshape(1,-1)
-#     predictions = model.predict(inputs).tolist()
-#     return jsonify({"predictions": predictions})
+@app.route("/predict/<input>",methods=['POST'])
+def predict(input):
+    data = [int(input)]
+    inputs = np.array(data).reshape(1,-1)
+    predictions = model.predict(inputs).tolist()
+    return jsonify({"predictions": predictions})
 
 
 if __name__ == "__main__":
